44ecc9019414913f1ddb6f433c4f385273392ee6
sjschrock <37678179+sjschrock@users.noreply.github.com>
2020-02-02 12:56:13 -0700

    add level 1 components

--- game/build/js/resources.js
+++ game/build/js/resources.js
@@ -40,6 +40,121 @@ game.resources = [
         "src": "data/img/map/area01_level_tiles.png"
     },
     {
+        "name": "Background2",
+        "type": "image",
+        "src": "data/img/map/Background2.png"
+    },
+    {
+        "name": "build_atlas",
+        "type": "image",
+        "src": "data/img/map/build_atlas.png"
+    },
+    {
+        "name": "castle_tileset_part1",
+        "type": "image",
+        "src": "data/img/map/castle_tileset_part1.png"
+    },
+    {
+        "name": "castle_tileset_part2",
+        "type": "image",
+        "src": "data/img/map/castle_tileset_part2.png"
+    },
+    {
+        "name": "castle_tileset_part3",
+        "type": "image",
+        "src": "data/img/map/castle_tileset_part3.png"
+    },
+    {
+        "name": "Castle2",
+        "type": "image",
+        "src": "data/img/map/Castle2.png"
+    },
+    {
+        "name": "colors",
+        "type": "image",
+        "src": "data/img/map/colors.png"
+    },
+    {
+        "name": "default_glass2",
+        "type": "image",
+        "src": "data/img/map/default_glass2.png"
+    },
+    {
+        "name": "doors_item_wood3",
+        "type": "image",
+        "src": "data/img/map/doors_item_wood3.png"
+    },
+    {
+        "name": "doors_trapdoor_side2",
+        "type": "image",
+        "src": "data/img/map/doors_trapdoor_side2.png"
+    },
+    {
+        "name": "doors_wood_a",
+        "type": "image",
+        "src": "data/img/map/doors_wood_a.png"
+    },
+    {
+        "name": "DungeonCrawl_ProjectUtumnoTileset",
+        "type": "image",
+        "src": "data/img/map/DungeonCrawl_ProjectUtumnoTileset.png"
+    },
+    {
+        "name": "hcolors",
+        "type": "image",
+        "src": "data/img/map/hcolors.png"
+    },
+    {
+        "name": "house_inside",
+        "type": "image",
+        "src": "data/img/map/house_inside.png"
+    },
+    {
+        "name": "hyptosis_sprites-and-tiles-for-you",
+        "type": "image",
+        "src": "data/img/map/hyptosis_sprites-and-tiles-for-you.png"
+    },
+    {
+        "name": "magical-potions-items-tileset",
+        "type": "image",
+        "src": "data/img/map/magical-potions-items-tileset.png"
+    },
+    {
+        "name": "obj_misk_atlas",
+        "type": "image",
+        "src": "data/img/map/obj_misk_atlas.png"
+    },
+    {
+        "name": "oppcastle-mod-tiles",
+        "type": "image",
+        "src": "data/img/map/oppcastle-mod-tiles.png"
+    },
+    {
+        "name": "oppcastle-tiles",
+        "type": "image",
+        "src": "data/img/map/oppcastle-tiles.png"
+    },
+    {
+        "name": "pngfuelcom",
+        "type": "image",
+        "src": "data/img/map/pngfuelcom.png"
+    },
+    {
+        "name": "ProjectUtumno_full",
+        "type": "image",
+        "src": "data/img/map/ProjectUtumno_full.png"
+    },
+    {
+        "name": "sun",
+        "type": "image",
+        "src": "data/img/map/sun.png"
+    },
+    {
+        "name": "wool_brown",
+        "type": "image",
+        "src": "data/img/map/wool_brown.png"
+    },
+    {
         "name": "gripe_run_right",
         "type": "image",
         "src": "data/img/sprite/gripe_run_right.png"
@@ -60,8 +175,133 @@ game.resources = [
         "src": "data/map/area01.tmx"
     },
     {
+        "name": "PrivetDrive",
+        "type": "tmx",
+        "src": "data/map/PrivetDrive.tmx"
+    },
+    {
         "name": "area01_level_tiles",
         "type": "tsx",
         "src": "data/map/area01_level_tiles.tsx"
+    },
+    {
+        "name": "Background2",
+        "type": "tsx",
+        "src": "data/map/Background2.tsx"
+    },
+    {
+        "name": "brick3",
+        "type": "tsx",
+        "src": "data/map/brick3.tsx"
+    },
+    {
+        "name": "brick4",
+        "type": "tsx",
+        "src": "data/map/brick4.tsx"
+    },
+    {
+        "name": "bricks",
+        "type": "tsx",
+        "src": "data/map/bricks.tsx"
+    },
+    {
+        "name": "bricks2",
+        "type": "tsx",
+        "src": "data/map/bricks2.tsx"
+    },
+    {
+        "name": "buildatlas",
+        "type": "tsx",
+        "src": "data/map/buildatlas.tsx"
+    },
+    {
+        "name": "castle_tileset_part1",
+        "type": "tsx",
+        "src": "data/map/castle_tileset_part1.tsx"
+    },
+    {
+        "name": "castle_tileset_part2",
+        "type": "tsx",
+        "src": "data/map/castle_tileset_part2.tsx"
+    },
+    {
+        "name": "castle_tileset_part3",
+        "type": "tsx",
+        "src": "data/map/castle_tileset_part3.tsx"
+    },
+    {
+        "name": "Castle2",
+        "type": "tsx",
+        "src": "data/map/Castle2.tsx"
+    },
+    {
+        "name": "colors",
+        "type": "tsx",
+        "src": "data/map/colors.tsx"
+    },
+    {
+        "name": "door",
+        "type": "tsx",
+        "src": "data/map/door.tsx"
+    },
+    {
+        "name": "door2",
+        "type": "tsx",
+        "src": "data/map/door2.tsx"
+    },
+    {
+        "name": "DungeonCrawl_ProjectUtumnoTileset",
+        "type": "tsx",
+        "src": "data/map/DungeonCrawl_ProjectUtumnoTileset.tsx"
+    },
+    {
+        "name": "garage2",
+        "type": "tsx",
+        "src": "data/map/garage2.tsx"
+    },
+    {
+        "name": "garagedoor",
+        "type": "tsx",
+        "src": "data/map/garagedoor.tsx"
+    },
+    {
+        "name": "glass",
+        "type": "tsx",
+        "src": "data/map/glass.tsx"
+    },
+    {
+        "name": "hcolors",
+        "type": "tsx",
+        "src": "data/map/hcolors.tsx"
+    },
+    {
+        "name": "obj_misk_atlas",
+        "type": "tsx",
+        "src": "data/map/obj_misk_atlas.tsx"
+    },
+    {
+        "name": "ProjectUtumno_full",
+        "type": "tsx",
+        "src": "data/map/ProjectUtumno_full.tsx"
+    },
+    {
+        "name": "roof",
+        "type": "tsx",
+        "src": "data/map/roof.tsx"
+    },
+    {
+        "name": "sun",
+        "type": "tsx",
+        "src": "data/map/sun.tsx"
+    },
+    {
+        "name": "windows",
+        "type": "tsx",
+        "src": "data/map/windows.tsx"
+    },
+    {
+        "name": "windows2",
+        "type": "tsx",
+        "src": "data/map/windows2.tsx"
     }
 ];
