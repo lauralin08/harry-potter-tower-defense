7c332bc59c97e2b276f06a63fe4856f449b7bff1
sjschrock <samanthajschrock@gmail.com>
2020-03-01 11:18:21 -0700

    add level 2, dementor, spider

---
+++ public/data/map/level2_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="level2_1" tilewidth="32" tileheight="32" tilecount="11375" columns="73">
+ <image source="../../AppData/Local/Packages/CanonicalGroupLimited.Ubuntu16.04onWindows_79rhkp1fndgsc/LocalState/rootfs/home/sjschrock/take2/harry-potter-tower-defense/public/data/img/map/level2_1.png" width="2342" height="4008"/>
+</tileset>
---
+++ public/data/map/level2_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="level2_2" tilewidth="32" tileheight="32" tilecount="1200" columns="50">
+ <image source="level2_2.png" width="1600" height="768"/>
+</tileset>
